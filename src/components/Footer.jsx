507759ed8113d83ae991f89e2ad520abee8a844d
Umar Daraz <134763608+UmarDaraz-26660@users.noreply.github.com>
2025-12-28 17:18:05 +0500

    Adjust Footer component layout and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
 
-    <div className="footer p-3 flex justify-center items-center  bg-amber-600 text-white fixed bottom-0 w-full">
+    <div className="footer h-25  p-3 flex justify-center items-center  bg-amber-600 text-white fixed bottom-0 w-full">
 
-          <div className='gap-5 p-2 flex flex-col md:flex-row justify-center items-center bg-blue-300 w-2xl rounded-2xl'>
+          <div className=' p-2 h-20 gap-2 flex flex-col md:flex-row justify-center items-center bg-blue-300 w-2xl rounded-2xl'>
 
 
         <h1 className='font-bold'>Developed By Umar Daraz</h1>
-        <button className='border-white border hover:font-bold text-sm p-3 rounded-2xl bg-blue-700'><a href="mailto:umar26660@gmail.com">Contact Me</a> </button>
+        <button className='border-white border hover:font-bold text-sm p-2 h-10 rounded-2xl  bg-blue-700'><a href="mailto:umar26660@gmail.com">Contact Me</a> </button>
       
 
       
