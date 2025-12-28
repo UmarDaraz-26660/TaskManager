738afc2e676ccfefe1a1bf7a997402012156bedd
Umar Daraz <134763608+UmarDaraz-26660@users.noreply.github.com>
2025-12-28 17:19:50 +0500

    Enhance Navbar with task management description
    Added a container with a description below the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
+    <>
+
     <nav className="bg-amber-600 p-3 flex justify-center items-center">
       <div> 
         <span className="text-3xl font-bold underline text-white"> Your Task Manager </span>  
@@ -10,6 +12,18 @@ const Navbar = () => {
        
       
     </nav>
+    
+
+
+    <div className="container  mx-auto p-6">
+
+        <p className="text-center mt-4 text-lg">Manage your tasks efficiently and stay organized!</p>
+
+
+      </div>
+    </>
+    
+    
   )
 }
 
